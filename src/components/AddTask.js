5b506ba2d2f590cd3109fc6c83a151b74815d851
Lucas Siviglia <siviglialucas@gmail.com>
2024-09-11 17:42:51 +1000

    adding state to task input & reset button

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,15 +1,43 @@
+import { useState } from "react";
 import './AddTask.css';
 
 export const AddTask = () => {
+  const [taskValue, setTaskValue] = useState("");
+  const [progress, setProgess] = useState(false);
+
+  const handleChange = (e) => {
+    setTaskValue(e.target.value);
+  }
+
+  const handleReset = () => {
+    setTaskValue("");
+    setProgess(false);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const task = {
+      id: Math.floor(Math.random() * 10000000),
+      name: taskValue,
+      completed: Boolean(progress)
+    }
+    handleReset();
+    console.log(task);
+  }
+
   return (
     <section className="addtask" >
-      <form>
-        <label htmlFor="task">
-          Task Name
-        </label>
-        <input type="text" name='task' id='task' placeholder='Task' autoComplete='off' />
-        <button type='submit' > Add Task</button>
+      <form onSubmit={handleSubmit} >
+        <input onChange={handleChange} type="text" name='task' id='task' placeholder='Task Name' autoComplete='off' value={taskValue} />
+        <select onChange={(event) => setProgess(event.target.value)} value={progress}>
+          <option value={false}>Pending</option>
+          <option value={true}>Completed</option>
+        </select>
+        <span onClick={handleReset} className="reset">Reset</span>
+        <button type='submit'>Add Task</button>
       </form>
+      {taskValue.length > 0 && <p>Length: {taskValue.length}</p>}
     </section>
   )
 }
